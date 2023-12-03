1f2b9ad05d13919ab740628fcb19224861062f57
LazY <darshitmistry2@gmail.com>
2023-12-03 11:30:03 +0530

    Dynamic Navbar was builted and other hand signout functionality was inited

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,6 +7,8 @@ import { buttonVariants } from "./ui/button";
 import Cart from "./Cart";
 import { getServerSideUser } from "@/lib/payload-utils";
 import { cookies} from "next/headers"
+import UserAccountNav from "./UserAccountNav";
+import { User } from "@/payload-types";
 
 const Navbar = async () => {
   const nextCookies = cookies()
@@ -42,10 +44,10 @@ const Navbar = async () => {
                     <span className="h-6 w-px bg-gray-200" aria-hidden="true" />
                   )}
                   {user ? (
-                    <p></p>
+                    <UserAccountNav user={user}/>
                   ) : (
                     <Link
-                      href="/sign-in"
+                      href="/sign-up"
                       className={buttonVariants({ variant: "ghost" })}
                     >
                       Create account
