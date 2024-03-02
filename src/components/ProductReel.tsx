3cf9fdeff59b2978568f904775f0c1b7b81192b1
LazY <darshitmistry2@gmail.com>
2024-03-02 09:07:58 +0530

    feat: cart checkout pages

--- src/components/ProductReel.tsx
+++ src/components/ProductReel.tsx
@@ -66,7 +66,7 @@ const ProductReel = (props: ProductReelProps) => {
         <div className="mt-6 flex items-center w-full">
           <div className="w-full grid grid-cols-2 gap-x-4 gap-y-10 sm:gap-x-6 md:grid-cols-4 md:gap-y-10 lg:gap-x-8">
             {map.map((products,i) => (
-              <ProductListing product={products} index={i}/>
+              <ProductListing key={`product-${i}`} product={products} index={i}/>
             ))}
           </div>
         </div>
